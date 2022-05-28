3be1cd89c5300e455139d55bf636ccadf690a544
eleksir <eleksir@exs-elm.ru>
2022-05-29 02:17:49 +0300

    [WIP] partially implement irc proto handler

--- go.mod
+++ go.mod
@@ -11,5 +11,8 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/thoj/go-ircevent v0.0.0-20210723090443-73e444401d64 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -13,20 +14,25 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/hjson/hjson-go"
 	log "github.com/sirupsen/logrus"
+	irc "github.com/thoj/go-ircevent"
 )
 
 // Конфиг
 type myConfig struct {
 	Redis struct {
-		Server  string `json:"server,omitempty"`
-		Port    int    `json:"port,omitempty"`
-		Channel string `json:"channel,omitempty"`
+		Server    string `json:"server,omitempty"`
+		Port      int    `json:"port,omitempty"`
+		Channel   string `json:"channel,omitempty"`
+		MyChannel string `json:"my_channel,omitempty"`
 	} `json:"redis"`
 	Irc struct {
 		Server    string   `json:"server,omitempty"`
 		Port      int      `json:"port,omitempty"`
 		Ssl       bool     `json:"ssl,omitempty"`
 		SslVerify bool     `json:"ssl_verify,omitempty"`
+		Nick      string   `json:"nick,omitempty"`
+		User      string   `json:"user,omitempty"`
+		Password  string   `json:"password,omitempty"`
 		Channels  []string `json:"channels"`
 	}
 	Loglevel    string `json:"loglevel,omitempty"`
@@ -78,6 +84,9 @@ var config myConfig
 
 // To break circular message forwarding we must set some sane default, it can be overridden via config
 var forwardMax int64 = 5
+
+// Объектики irc-клиента
+var ircClient *irc.Connection
 
 // Объектики клиента-редиски
 var redisClient *redis.Client
@@ -169,9 +178,46 @@ func readConfig() {
 
 		if sampleConfig.Redis.Channel == "" {
 			log.Errorf("Channel field in config file %s must be set", location)
+			os.Exit(1)
 		}
 
-		// TODO: Set/validate irc vars
+		if sampleConfig.Redis.MyChannel == "" {
+			log.Errorf("My_channel field in config file %s must be set", location)
+			os.Exit(1)
+		}
+
+		// Значения для IRC-клиента
+		if sampleConfig.Irc.Server == "" {
+			sampleConfig.Irc.Server = "localhost"
+			log.Errorf("Irc server is not defined in config, using localhost")
+		}
+
+		if sampleConfig.Irc.Port == 0 {
+			sampleConfig.Irc.Port = 6667
+			log.Infof("Irc port is not defined in config, using 6667")
+		}
+
+		if sampleConfig.Irc.Ssl != true {
+			sampleConfig.Irc.SslVerify = false
+		}
+
+		if sampleConfig.Irc.Nick == "" {
+			log.Errorf("Irc nick is not defined in config, quitting")
+			os.Exit(1)
+		}
+
+		if sampleConfig.Irc.User == "" {
+			sampleConfig.Irc.User = sampleConfig.Irc.Nick
+		}
+
+		// Если sampleConfig.Irc.Password не задан, то авторизации через Nickserv не будет
+
+		// Нам бот нужен на каких-то IRC-каналах, а не "просто так"
+		if len(sampleConfig.Irc.Channels) < 1 {
+			log.Errorf("No irc channels defined in config, quitting")
+			os.Exit(1)
+		}
+
 		if sampleConfig.Loglevel == "" {
 			sampleConfig.Loglevel = "info"
 		}
@@ -197,7 +243,7 @@ func readConfig() {
 	}
 }
 
-// Хэндлер сигналов закрывает все бд и сваливает из приложения
+// Хэндлер сигналов закрывает все бд, все сетевые соединения и сваливает из приложения
 func sigHandler() {
 	var err error
 
@@ -228,13 +274,145 @@ func sigHandler() {
 			log.Errorf("Unable to close redis connection cleanly: %s", err)
 		}
 
+		log.Info("Quitting")
+		ircClient.Quit()
 		os.Exit(0)
 	}
 }
 
-// Горутинка, которая парсит json-чики прилетевшие из REDIS-ки
+// msgParser парсит json-чики прилетевшие из REDIS-ки
 func msgParser(msg string) {
 	return
+}
+
+// ircClientInit горутинка для работы с протоколом irc
+func ircClientRun() {
+	for {
+		// Иницализируем irc-клиента
+		ircClient = irc.IRC(config.Irc.Nick, config.Irc.User)
+
+		if config.Irc.Ssl {
+			ircClient.UseTLS = true
+
+			if !config.Irc.SslVerify {
+				ircClient.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+			}
+		}
+
+		// Навесим коллбэков на все возможные и невозможные error status code, которые мы можем получить и сдампим это
+		// дело в лог. https://datatracker.ietf.org/doc/html/rfc1459 и https://datatracker.ietf.org/doc/html/rfc2812
+		ircClient.AddCallback("401", func(e *irc.Event) {
+			log.Errorf("401 ERR_NOSUCHNICK, %s", e.Raw)
+		})
+		ircClient.AddCallback("403", func(e *irc.Event) {
+			log.Errorf("403 ERR_NOSUCHCHANNEL, %s", e.Raw)
+		})
+		ircClient.AddCallback("404", func(e *irc.Event) {
+			log.Errorf("404 ERR_CANNOTSENDTOCHAN, %s", e.Raw)
+		})
+		ircClient.AddCallback("405", func(e *irc.Event) {
+			log.Errorf("405 ERR_TOOMANYCHANNELS, %s", e.Raw)
+		})
+		ircClient.AddCallback("407", func(e *irc.Event) {
+			log.Errorf("407 ERR_TOOMANYTARGETS, %s", e.Raw)
+		})
+		ircClient.AddCallback("411", func(e *irc.Event) {
+			log.Errorf("411 ERR_NORECIPIENT, %s", e.Raw)
+		})
+		ircClient.AddCallback("412", func(e *irc.Event) {
+			log.Errorf("412 ERR_NOTEXTTOSEND, %s", e.Raw)
+		})
+		ircClient.AddCallback("421", func(e *irc.Event) {
+			log.Errorf("421 ERR_UNKNOWNCOMMAND, %s", e.Raw)
+		})
+		ircClient.AddCallback("431", func(e *irc.Event) {
+			log.Errorf("431 ERR_NONICKNAMEGIVEN, %s", e.Raw)
+		})
+		ircClient.AddCallback("432", func(e *irc.Event) {
+			log.Errorf("432 ERR_ERRONEUSNICKNAME, %s", e.Raw)
+		})
+		ircClient.AddCallback("433", func(e *irc.Event) {
+			log.Errorf("433 ERR_NICKNAMEINUSE, %s", e.Raw)
+		})
+		ircClient.AddCallback("436", func(e *irc.Event) {
+			log.Errorf("436 ERR_NICKCOLLISION, %s", e.Raw)
+		})
+		ircClient.AddCallback("437", func(e *irc.Event) {
+			log.Errorf("437 ERR_UNAVAILRESOURCE, %s", e.Raw)
+		})
+		ircClient.AddCallback("441", func(e *irc.Event) {
+			log.Errorf("441 ERR_USERNOTINCHANNEL, %s", e.Raw)
+		})
+		ircClient.AddCallback("442", func(e *irc.Event) {
+			log.Errorf("442 ERR_NOTONCHANNEL, %s", e.Raw)
+		})
+		ircClient.AddCallback("451", func(e *irc.Event) {
+			log.Errorf("451 ERR_NOTREGISTERED, %s", e.Raw)
+		})
+		ircClient.AddCallback("461", func(e *irc.Event) {
+			log.Errorf("461 ERR_NEEDMOREPARAMS, %s", e.Raw)
+		})
+		ircClient.AddCallback("462", func(e *irc.Event) {
+			log.Errorf("462 ERR_ALREADYREGISTRED, %s", e.Raw)
+		})
+		ircClient.AddCallback("464", func(e *irc.Event) {
+			log.Errorf("464 ERR_PASSWDMISMATCH, %s", e.Raw)
+		})
+		ircClient.AddCallback("465", func(e *irc.Event) {
+			log.Errorf("465 ERR_YOUREBANNEDCREEP, %s", e.Raw)
+		})
+		ircClient.AddCallback("471", func(e *irc.Event) {
+			log.Errorf("471 ERR_CHANNELISFULL, %s", e.Raw)
+		})
+		ircClient.AddCallback("472", func(e *irc.Event) {
+			log.Errorf("472 ERR_UNKNOWNMODE, %s", e.Raw)
+		})
+		ircClient.AddCallback("473", func(e *irc.Event) {
+			log.Errorf("473 ERR_INVITEONLYCHAN, %s", e.Raw)
+		})
+		ircClient.AddCallback("474", func(e *irc.Event) {
+			log.Errorf("474 ERR_BANNEDFROMCHAN, %s", e.Raw)
+		})
+		ircClient.AddCallback("477", func(e *irc.Event) {
+			log.Errorf("477 ERR_NOCHANMODES, %s", e.Raw)
+		})
+		ircClient.AddCallback("478", func(e *irc.Event) {
+			log.Errorf("478 ERR_BANLISTFULL, %s", e.Raw)
+		})
+		ircClient.AddCallback("482", func(e *irc.Event) {
+			log.Errorf("482 ERR_CHANOPRIVSNEEDED, %s", e.Raw)
+		})
+		ircClient.AddCallback("484", func(e *irc.Event) {
+			log.Errorf("484 ERR_RESTRICTED, %s", e.Raw)
+		})
+		ircClient.AddCallback("485", func(e *irc.Event) {
+			log.Errorf("485 ERR_UNIQOPPRIVSNEEDED, %s", e.Raw)
+		})
+		ircClient.AddCallback("501", func(e *irc.Event) {
+			log.Errorf("501 ERR_UMODEUNKNOWNFLAG, %s", e.Raw)
+		})
+		ircClient.AddCallback("502", func(e *irc.Event) {
+			log.Errorf("502 ERR_USERSDONTMATCH, %s", e.Raw)
+		})
+
+		// По выходу из клиента
+		ircClient.AddCallback("376", func(e *irc.Event) {
+			log.Info("Quitting")
+			ircClient.Quit()
+		})
+
+		// Сделаем уже что-то полезное!
+		ircClient.AddCallback("001", func(e *irc.Event) {
+			for _, channel := range config.Irc.Channels {
+				ircClient.Join(channel)
+			}
+		})
+
+		// Здесь у нас парсер сообщений из IRC
+		ircClient.AddCallback("PRIVMSG", func(e *irc.Event) {
+			return
+		})
+	}
 }
 
 // Производит некоторую инициализацию перед запуском main()
@@ -281,7 +459,7 @@ func main() {
 		log.SetOutput(logfile)
 	}
 
-	// Иницализируем клиента
+	// Иницализируем redis-клиента
 	redisClient = redis.NewClient(&redis.Options{
 		Addr: fmt.Sprintf("%s:%d", config.Redis.Server, config.Redis.Port),
 	})
@@ -289,6 +467,8 @@ func main() {
 	log.Debugf("Lazy connect() to redis at %s:%d", config.Redis.Server, config.Redis.Port)
 	subscriber = redisClient.Subscribe(ctx, config.Redis.Channel)
 	redisMsgChan := subscriber.Channel()
+
+	go ircClientRun()
 
 	// Самое время поставить траппер сигналов
 	signal.Notify(sigChan,
